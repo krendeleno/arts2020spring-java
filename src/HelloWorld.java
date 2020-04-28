9ff39278a1814baeedfb08fef41504021357742f
konnovK <konnov_k@protonmail.com>
2020-04-28 22:39:47 +0300

    изменил HelloWorld из "дома"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Привет из \"Университета\"!!!");
+        System.out.println("Привет теперь уже из \"дома\"!!!");
     }
 }
